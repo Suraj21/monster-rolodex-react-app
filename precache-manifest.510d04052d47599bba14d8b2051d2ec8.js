4684ecca2a75760caa514ddc61e976b2a402ee6e
sachidanand suraj <suraj.thepions@gmail.com>
2020-06-13 17:21:18 +0530

    Updates

---
+++ precache-manifest.510d04052d47599bba14d8b2051d2ec8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec0feabc6a8872bdbaddd13fcf327309",
+    "url": "/monster-rolodex-react-app/index.html"
+  },
+  {
+    "revision": "25173810876e938ebc47",
+    "url": "/monster-rolodex-react-app/static/css/main.19a225e5.chunk.css"
+  },
+  {
+    "revision": "9f834d1fc4df12fb31cd",
+    "url": "/monster-rolodex-react-app/static/js/2.308474c0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-rolodex-react-app/static/js/2.308474c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25173810876e938ebc47",
+    "url": "/monster-rolodex-react-app/static/js/main.e4f901f5.chunk.js"
+  },
+  {
+    "revision": "1bedaec91eb2587f6e0e",
+    "url": "/monster-rolodex-react-app/static/js/runtime-main.7d222dc1.js"
+  }
+]);
